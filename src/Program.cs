28dc3c4ab9eec381e05e350eff19f668ce54c734
Jacob Misirian <misirianrule@gmail.com>
2015-11-11 12:02:32 -0600

    Added catching FileNotFound and removed trailing Read()

--- src/Program.cs
+++ src/Program.cs
@@ -15,96 +15,102 @@ namespace CrackerJac
 
         static void Main(string[] args)
         {
-            if (args.Length <= 0)
-                args = new string[1] { "--help" };
-
-            switch(args[0].ToLower())
+            try
             {
-                case "-h":
-                case "--help":
-                    Console.WriteLine("CrackerJac.exe [OPTIONS] [HASH_FILE] [DICTIONARY_FILE]");
-                    Console.WriteLine("Options:");
-                    Console.WriteLine("-gu --generate-unsalted [STRING]\tGenerates an unsalted hash from the next string.");
-                    Console.WriteLine("-gs --generate-salted [STRING] [SALT]\tGenerates a salted hash from the next two strings.");
-                    Console.WriteLine("-h --help\tDisplays this help and exits.");
-                    Console.WriteLine("-m --mybb\tCracks salted MyBB style passwords.");
-                    Console.WriteLine("-s --search [QUERY] [DICTIONARY_FILE]\tSearches the dictionary file to see if query exists.");
-                    Environment.Exit(0);
-                    break;
-                case "-m":
-                case "-mybb":
-                    if (args.Length < 3)
-                    {
-                        Console.WriteLine("Missing [HASH_FILE] or [DICTIONARY_FILE] after option " + args[0]);
+                if (args.Length <= 0)
+                    args = new string[1] { "--help" };
+
+                switch (args[0].ToLower())
+                {
+                    case "-h":
+                    case "--help":
+                        Console.WriteLine("CrackerJac.exe [OPTIONS] [HASH_FILE] [DICTIONARY_FILE]");
+                        Console.WriteLine("Options:");
+                        Console.WriteLine("-gu --generate-unsalted [STRING]\tGenerates an unsalted hash from the next string.");
+                        Console.WriteLine("-gs --generate-salted [STRING] [SALT]\tGenerates a salted hash from the next two strings.");
+                        Console.WriteLine("-h --help\tDisplays this help and exits.");
+                        Console.WriteLine("-m --mybb\tCracks salted MyBB style passwords.");
+                        Console.WriteLine("-s --search [QUERY] [DICTIONARY_FILE]\tSearches the dictionary file to see if query exists.");
                         Environment.Exit(0);
-                    }
-                    mybb = true;
-                    hashLocation = args[1];
-                    dictionaryLocation = args[2];
-                    break;
-                case "-gu":
-                case "--generate-unsalted":
-                    if (args.Length < 2)
-                    {
-                        Console.WriteLine("String must follow " + args[0]);
+                        break;
+                    case "-m":
+                    case "-mybb":
+                        if (args.Length < 3)
+                        {
+                            Console.WriteLine("Missing [HASH_FILE] or [DICTIONARY_FILE] after option " + args[0]);
+                            Environment.Exit(0);
+                        }
+                        mybb = true;
+                        hashLocation = args[1];
+                        dictionaryLocation = args[2];
+                        break;
+                    case "-gu":
+                    case "--generate-unsalted":
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("String must follow " + args[0]);
+                            Environment.Exit(0);
+                        }
+                        Console.WriteLine(HashCracker.Md5(args[1]));
                         Environment.Exit(0);
-                    }
-                    Console.WriteLine(HashCracker.Md5(args[1]));
-                    Environment.Exit(0);
-                    break;
-                case "-gs":
-                case "--generate-salted":
-                    if (args.Length < 3)
-                    {
-                        Console.WriteLine("String and Salt must follow " + args[0]);
+                        break;
+                    case "-gs":
+                    case "--generate-salted":
+                        if (args.Length < 3)
+                        {
+                            Console.WriteLine("String and Salt must follow " + args[0]);
+                            Environment.Exit(0);
+                        }
+                        Console.WriteLine(HashCracker.Md5(HashCracker.Md5(args[2]) + HashCracker.Md5(args[1])));
                         Environment.Exit(0);
-                    }
-                    Console.WriteLine(HashCracker.Md5(HashCracker.Md5(args[2]) + HashCracker.Md5(args[1])));
-                    Environment.Exit(0);
-                    break;
-                case "-s":
-                case "--search":
-                    if (args.Length < 3)
-                    {
-                        Console.WriteLine("Query and [DICTIONARY_FILE] must follow " + args[0]);
+                        break;
+                    case "-s":
+                    case "--search":
+                        if (args.Length < 3)
+                        {
+                            Console.WriteLine("Query and [DICTIONARY_FILE] must follow " + args[0]);
+                            Environment.Exit(0);
+                        }
+                        if (HashCracker.Exists(args[1], args[2]))
+                            Console.WriteLine(args[1] + " was found in dictionary.");
+                        else
+                            Console.WriteLine(args[1] + " was not found in dictionary.");
                         Environment.Exit(0);
-                    }
-                    if (HashCracker.Exists(args[1], args[2]))
-                        Console.WriteLine(args[1] + " was found in dictionary.");
+                        break;
+                    default:
+                        if (args[0].StartsWith("-"))
+                        {
+                            Console.WriteLine("Unknown option: " + args[0]);
+                            Environment.Exit(0);
+                        }
+                        if (args.Length < 2)
+                        {
+                            Console.WriteLine("Missing [HASH_FILE] or [DICTIONARY_FILE]");
+                            Environment.Exit(0);
+                        }
+                        hashLocation = args[0];
+                        dictionaryLocation = args[1];
+                        break;
+                }
+                string[] hashes = File.ReadAllLines(hashLocation);
+                foreach (string entry in hashes)
+                {
+                    string line = entry.Trim();
+                    if (line.StartsWith("#") || line == "" || line == "\n")
+                        continue;
+
+                    string[] parts = line.Split(' ');
+                    if (mybb)
+                        new Task(() => userThread(parts[0], parts[1], parts[2])).Start();
                     else
-                        Console.WriteLine(args[1] + " was not found in dictionary.");
-                    Environment.Exit(0);
-                    break;
-                default:
-                    if (args[0].StartsWith("-"))
-                    {
-                        Console.WriteLine("Unknown option: " + args[0]);
-                        Environment.Exit(0);
-                    }
-                    if (args.Length < 2)
-                    {
-                        Console.WriteLine("Missing [HASH_FILE] or [DICTIONARY_FILE]");
-                        Environment.Exit(0);
-                    }
-                    hashLocation = args[0];
-                    dictionaryLocation = args[1];
-                    break;
+                        new Task(() => userThread(parts[0], parts[1])).Start();
+                }
             }
-
-            string[] hashes = File.ReadAllLines(hashLocation);
-            foreach (string entry in hashes)
+            catch (FileNotFoundException ex)
             {
-                string line = entry.Trim();
-                if (line.StartsWith("#") || line == "" || line == "\n")
-                    continue;
-
-                string[] parts = line.Split(' ');
-                if (mybb)
-                    new Task(() => userThread(parts[0], parts[1], parts[2])).Start();
-                else
-                    new Task(() => userThread(parts[0], parts[1])).Start();
+                Console.WriteLine(ex.FileName + " was not found! Try running CrackerJac --help for help");
+                Environment.Exit(0);
             }
-            Console.Read();
         }
 
         private static void userThread(string name, string hash, string salt = "")
