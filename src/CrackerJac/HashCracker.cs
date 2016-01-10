274c7c2bb363a6e217459b60dfab26da11e1c1b5
Jacob Misirian <misirianrule@gmail.com>
2016-01-10 14:23:43 -0600

    Added methods to HashCracker.cs that use a string[] for dictionary.

--- src/CrackerJac/HashCracker.cs
+++ src/CrackerJac/HashCracker.cs
@@ -11,11 +11,22 @@ namespace CrackerJac
         public static string HashingMethod { get { return hashingMethod; } set { hashingMethod = value; } }
         private static string hashingMethod = "MD5";
 
+        public string DictionaryAttackUnsalted(string hash, string[] dictionary, string append = "")
+        {
+            foreach (string entry in dictionary)
+            {
+                string word = (append == "") ? entry : entry + append;
+                if (Hash(word) == hash)
+                    return word;
+            }
+
+            return "";
+        }
+
         public string DictionaryAttackUnsalted(string hash, string dictionaryPath, string append = "")
         {
             return DictionaryAttackUnsalted(hash, new StreamReader(dictionaryPath).BaseStream, append);
         }
-
         public string DictionaryAttackUnsalted(string hash, Stream dictionary, string append = "")
         {
             StreamReader reader = new StreamReader(dictionary);
@@ -29,11 +40,26 @@ namespace CrackerJac
             return "";
         }
 
+        public string DictionaryAttackUnsaltedAppend(string hash, string[] dictionary, int appendFrom, int appendTo)
+        {
+            string tryWithoutAppend = DictionaryAttackUnsalted(hash, dictionary);
+            if (tryWithoutAppend != "")
+                return tryWithoutAppend;
+
+            while (appendFrom <= appendTo)
+            {
+                string res = DictionaryAttackUnsalted(hash, dictionary, appendFrom++.ToString());
+                if (res != "")
+                    return res;
+            }
+
+            return "";
+        }
+
         public string DictionaryAttackUnsaltedAppend(string hash, string dictionaryPath, int appendFrom, int appendTo)
         {
             return DictionaryAttackUnsaltedAppend(hash, new StreamReader(dictionaryPath).BaseStream, appendFrom, appendTo);
         }
-
         public string DictionaryAttackUnsaltedAppend(string hash, Stream dictionary, int appendFrom, int appendTo)
         {
             string tryWithoutAppend = DictionaryAttackUnsalted(hash, dictionary);
@@ -50,11 +76,22 @@ namespace CrackerJac
             return "";
         }
 
+        public string DictionaryAttackSalted(string hash, string salt, string[] dictionary, string append = "")
+        {
+            foreach (string entry in dictionary)
+            {
+                string word = (append == "") ? entry : entry + append;
+                if (Hash(Hash(salt) + Hash(word)) == hash)
+                    return word;
+            }
+
+            return "";
+        }
+
         public string DictionaryAttackSalted(string hash, string salt, string dictionaryPath, string append = "")
         {
             return DictionaryAttackSalted(hash, salt, new StreamReader(dictionaryPath).BaseStream, append);
         }
-
         public string DictionaryAttackSalted(string hash, string salt, Stream dictionary, string append = "")
         {
             
@@ -69,11 +106,26 @@ namespace CrackerJac
             return "";
         }
 
+        public string DictionaryAttackSaltedAppend(string hash, string salt, string[] dictionary, int appendFrom, int appendTo)
+        {
+            string tryWithoutAppend = DictionaryAttackSalted(hash, salt, dictionary);
+            if (tryWithoutAppend != "")
+                return tryWithoutAppend;
+
+            while (appendFrom <= appendTo)
+            {
+                string res = DictionaryAttackSalted(hash, salt, dictionary, appendFrom++.ToString());
+                if (res != "")
+                    return res;
+            }
+
+            return "";
+        }
+
         public string DictionaryAttackSaltedAppend(string hash, string salt, string dictionaryPath, int appendFrom, int appendTo)
         {
             return DictionaryAttackSaltedAppend(hash, salt, new StreamReader(dictionaryPath).BaseStream, appendFrom, appendTo);
         }
-
         public string DictionaryAttackSaltedAppend(string hash, string salt, Stream dictionary, int appendFrom, int appendTo)
         {
             string tryWithoutAppend = DictionaryAttackSalted(hash, salt, dictionary);
@@ -94,12 +146,10 @@ namespace CrackerJac
         {
             return bruteForce(hash, letters, maxLength);
         }
-
         public string BruteforceAttackSalted(string hash, string salt, string letters, int maxLength)
         {
             return bruteForce(hash, letters, maxLength, salt);
         }
-
         private string bruteForce(string hash, string letters, int maxLength, string salt = "")
         {
             char firstLetter = letters.First();
